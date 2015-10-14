64301dd8a2d1ea05785e07aa6950c48394567a58
Hirofum Hayashi <pica.hayashi@gmail.com>
2015-10-14 10:41:18 +0900

    Create mapswipetool_ja.ts
    Need to fix line numbers if source code change.

---
+++ i18n/mapswipetool_ja.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ja_JP" sourcelanguage="en">
+<context>
+    <name>MapSwipeTool</name>
+    <message>
+        <location filename="../mapswipetool.py" line="54"/>
+        <source>Select active Layer or Group(with layers)  in legend.</source>
+        <translation>レイヤパネルのアクティブレイヤまたはグループを選択します</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="103"/>
+        <source>Active layer is '%s'.</source>
+        <translation>アクティブレイヤは '%s'.</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="109"/>
+        <source>Active group is '%s'.</source>
+        <translation>アクティブグループは '%s'.</translation>
+    </message>
+</context>
+</TS>
