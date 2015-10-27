55b8f340f086000d5cf8e06a65254a80ab7c1e8f
Hirofum Hayashi <pica.hayashi@gmail.com>
2015-10-27 11:32:33 +0900

    Update mapswipetool_ja.ts
    Change line numbers

--- i18n/mapswipetool_ja.ts
+++ i18n/mapswipetool_ja.ts
@@ -4,17 +4,17 @@
 <context>
     <name>MapSwipeTool</name>
     <message>
-        <location filename="../mapswipetool.py" line="58"/>
+        <location filename="../mapswipetool.py" line="92"/>
         <source>Select active Layer or Group(with layers) in legend.</source>
         <translation>レイヤパネルのアクティブレイヤまたはグループを選択します.</translation>
     </message>
     <message>
-        <location filename="../mapswipetool.py" line="107"/>
+        <location filename="../mapswipetool.py" line="109"/>
         <source>Active layer is &apos;%s&apos;.</source>
         <translation>アクティブレイヤは &apos;%s&apos;.</translation>
     </message>
     <message>
-        <location filename="../mapswipetool.py" line="113"/>
+        <location filename="../mapswipetool.py" line="115"/>
         <source>Active group is &apos;%s&apos;.</source>
         <translation>アクティブグループは &apos;%s&apos;.</translation>
     </message>
